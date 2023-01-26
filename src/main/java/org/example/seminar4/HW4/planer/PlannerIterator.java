ea0ad6b3969edf07c1d8ef5a1b6ec66fb3753b5f
Dmitrii <my@dimka89.ru>
2023-01-26 16:22:23 +0300

    lesson4HW

---
+++ src/main/java/org/example/seminar4/HW4/planer/PlannerIterator.java
@@ -0,0 +1,26 @@
+package org.example.seminar4.HW4.planer;
+
+import java.util.Iterator;
+
+
+public class PlannerIterator implements Iterator<Task> {
+
+
+    private int index = -1;
+    private final Planner tasks;
+
+    public PlannerIterator(Planner tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < tasks.getSize()-1;
+    }
+
+    @Override
+    public Task next() {
+        return tasks.getTask(++index);
+    }
+
+}
